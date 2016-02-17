654da153678ab0405c8d7228b2d0165d99ece637
Shenglan Qiao <shenglan@stanford.edu>
2016-02-17 11:27:06 -0800

    writing polar intensities, q values, phi values, and number of molecules to file

--- intensity_profile.py
+++ intensity_profile.py
@@ -15,29 +15,59 @@
 ##############################################################################
 
 import numpy as np
-import mdtraj as md
+import mdtraj
+import h5py
+import os
 
 from thor import xray
-import matlibplot.pyplot as plt
+import matplotlib.pyplot as plt
 ##############################################################################
 # Code
 ##############################################################################
-t = mdtraj.load("nanosphere/50.0m1.pdb")
+sphere = "50.0_m1"
+t=mdtraj.load('nanosphere/'+sphere+'.pdb')
 
-
-n_shots = 100                         # total number of shots to do
-n_molecules = 1                     # the number of molecules to include per shot
-q_values = np.arange(1.2,2.4,0.05)  # the |q| values of the rings to sim
+# simulation parameters
+n_shots = 500                      # total number of shots to do
+n_molecules = 512                     # the number of molecules to include per shot
+q_values = [1.61]  # the |q| values of the rings to sim
 n_phi = 360                         # number of pts around the rings
 
+
+output_file = "simulated_data/"+sphere+"_s"+str(n_shots)+'.hd5f'
+while os.path.isfile(output_file):
+    print "Will not overwrite old file. Please enter new name:"
+    outpu_file = rawinput()
+
+
+# do the simulations
 rings = xray.Rings.simulate(t, n_molecules, q_values, n_phi, n_shots)
 
-# check the WAXS
-plt.figure()
+# data to save
+PI=rings.polar_intensities
+phis=rings.phi_values
+qs=rings.q_values
 
-waxs = rings.intensity_profile() # first col is |q|, second is intensity
+# save the data
+f = h5py.File(output_file,'w')
+f.create_dataset('polar_intensities',data=PI)
+f.create_dataset('phi_values',data=phis)
+f.create_dataset('q_values',data=qs)
+f['polar_intensities'].attrs.create('num_molecules',n_molecules)
 
-plt.plot(waxs[:,0], waxs[:,1], lw=2)
-plt.xlabel(r'$q / \AA^{-1}$')
-plt.ylabel('Intensity')
-plt.show()
+f.close()
+
+
+
+
+# ring.save(output_file)
+# 
+# # check the WAXS
+# plt.figure()
+# 
+# waxs = rings.intensity_profile() # first col is |q|, second is intensity
+# 
+# plt.plot(waxs[:,0], waxs[:,1], lw=2)
+# plt.xlabel(r'$q / \AA^{-1}$')
+# plt.ylabel('Intensity')
+# plt.show()
